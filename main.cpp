f9615cbbcf7ee34c734a57e1bb18e2e54a5eb773
Umar Gul <107802247+StormTrooper10@users.noreply.github.com>
2023-07-28 01:53:13 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,15 @@
 
-#include "Diablo.h"
+#include "TV.h"
+#include "Remote.h"
+#include <cstring>
 #include <iostream>
 using namespace std;
 
 int main() {
 
-	Diablo obj1, obj2;
+	Remote obj1;
 
-	obj1.set_color("Rose Gold & Black");
-	obj1.set_cubic_capacity(8655);
-	obj1.set_seats(4);
-	obj1.set_owner_name("Sanna");
-	obj1.set_manufacture_year(2007);
-	obj1.set_frame_number("88672");
-	obj1.set_engine_number("UH765");
+	cout<<obj1.change_channel(89);
 
-	obj2=obj1;
-
-	obj2.set_owner_name("Umar");
-	obj2.set_manufacture_year(9832);
-	obj2.set_frame_number("89UH");
-	obj2.set_engine_number("88HUH");
-
-	cout<<obj2.get_color()<<endl;
-
-	obj2.set_color("Black");
-
-	cout<<obj2.get_color()<<endl;
-	cout<<obj1.get_color()<<endl;
-
-	return 0;
+return 0;
 }
